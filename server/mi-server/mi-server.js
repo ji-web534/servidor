c34b143308139db9bd285211619522ea24b64877
ji-web534 <239822821+ji-web534@users.noreply.github.com>
2026-04-18 23:55:24 -0300

    debugeo de las const duplicados

--- server/mi-server/mi-server.js
+++ server/mi-server/mi-server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
-const app = express();
-const PORT = 4000;
+
 
 // . Le damos permiso a React para entrar
 const app = express();
